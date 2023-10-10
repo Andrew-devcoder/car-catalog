9fb6ed4193602b368f265c3020e8b577d8b1e20e
Andrew-devcoder <andrey.kovpak.01@gmail.com>
2023-10-10 15:56:09 +0300

    add module for jsx

--- src/components/screens/home/Home.jsx
+++ src/components/screens/home/Home.jsx
@@ -1,8 +1,19 @@
+
+import styles from "./Home.module.css";
+
 const Home = () => {
 	return <div>
 		<h1>
 			Catalog cars
 		</h1>
+
+		<div>
+			<div className={styles.item}>
+				<h2>Car 1</h2>
+				<p>$100 000</p>
+				<button>Read more</button>
+			</div>
+		</div>
 	</div>
 }
 
