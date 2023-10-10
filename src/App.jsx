27a3f31793e52c21bb27c0415451890204505ee6
Andrew-devcoder <andrey.kovpak.01@gmail.com>
2023-10-10 13:57:45 +0300

    add sass

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+import '../src/sass/App.scss'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-      <div>
-       <h1>
+    <div>
+      <h1>
         Catalog car
-       </h1>
-      </div>
+      </h1>
+    </div>
   )
 }
 
